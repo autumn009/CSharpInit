291edc3f8d1929f6622d198f1e177c07dd363a32
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 08:30:31 +0900

    add more

--- Inherit/Inherit/Program.cs
+++ Inherit/Inherit/Program.cs
@@ -1,14 +1,29 @@
 ﻿var x = new C();
 
+static class Util
+{
+    internal static string Sub(string x)
+    {
+        Console.WriteLine($"Sub({x}) called");
+        return x;
+    }
+}
+
 class A
 {
+    internal string x1 = Util.Sub("x1");
+    internal string y1 { get; set; } = Util.Sub("y1");
     internal A() => Console.WriteLine("A constructed");
 }
 class B:A
 {
+    internal string x2 = Util.Sub("x2");
+    internal string y2 { get; set; } = Util.Sub("y2");
     internal B() => Console.WriteLine("B constructed");
 }
 class C:B
 {
+    internal string x3 = Util.Sub("x3");
+    internal string y3 { get; set; } = Util.Sub("y3");
     internal C() => Console.WriteLine("C constructed");
 }
---
+++ Inherit2/Inherit2/Program.cs
@@ -0,0 +1,12 @@
+﻿var x = new B();
+
+class A
+{
+    protected virtual int MyProperty { get; set; } = 1;
+    internal A() => Console.WriteLine($"A constructed MyProperty={MyProperty}");
+}
+class B : A
+{
+    protected override int MyProperty { get; set; } = 2;
+    internal B() => Console.WriteLine($"B constructed MyProperty={MyProperty}");
+}

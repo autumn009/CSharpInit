a7fe71e4a5f60965a9c6ffe58e86132e760a364b
Akira Kawamata <autumn@piedey.co.jp>
2023-08-15 16:42:04 +0900

    add BadCase1/2

---
+++ BadCase1/BadCase1/Program.cs
@@ -0,0 +1,15 @@
+﻿var x1 = new A();
+var x2 = new B();
+
+class A
+{
+    public string a = "A";
+    public virtual void Sub() => Console.WriteLine($"({a})");
+    public A() => Sub();
+}
+
+class B : A
+{
+    public override void Sub() => Console.WriteLine($"[{a}]");
+    public B() => a = "B";
+}
---
+++ BadCase2/BadCase2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"A:{B.MyProperty}");
+
+static class A
+{
+    public static int MyProperty { get; set; } = 123;
+    static A() => Console.WriteLine($"B:{B.MyProperty}");
+}
+
+static class B
+{
+    public static int MyProperty { get; set; } = A.MyProperty;
+    static B() => Console.WriteLine($"C:{A.MyProperty}");
+}
+

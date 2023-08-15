77bfafe714d6a318f8b76bbd06e3262b47dd544b
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 08:06:57 +0900

    add Ingerit

---
+++ Inherit/Inherit/Program.cs
@@ -0,0 +1,14 @@
+﻿var x = new C();
+
+class A
+{
+    internal A() => Console.WriteLine("A constructed");
+}
+class B:A
+{
+    internal B() => Console.WriteLine("B constructed");
+}
+class C:B
+{
+    internal C() => Console.WriteLine("C constructed");
+}

830b9bd97bb339ce0b57d31ebbd549a1490e87eb
Akira Kawamata <autumn@piedey.co.jp>
2023-08-15 19:34:32 +0900

    add InitMembers

---
+++ InitMembers/InitMembers/Program.cs
@@ -0,0 +1,18 @@
+﻿var x = new X() { D = 3 };
+Console.WriteLine($"x.A={x.A}");
+Console.WriteLine($"x.B={x.B}");
+Console.WriteLine($"x.C={x.C}");
+Console.WriteLine($"x.D={x.D}");
+
+class X
+{
+    internal int A;
+    internal int B=1;
+    internal int C=1;
+    internal int D=1;
+    public X()
+    {
+        C = 2;
+        D = 2;
+    }
+}

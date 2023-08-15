fa6a99218f53bf54f1640e2fb572b45d1df10a41
Akira Kawamata <autumn@piedey.co.jp>
2023-08-15 18:16:44 +0900

    fix case

--- FieldProps/FieldProps/Program.cs
+++ FieldProps/FieldProps/Program.cs
@@ -1,10 +1,12 @@
-﻿Console.WriteLine(X.A);
+﻿Console.WriteLine($"X.A={X.A}");
+Console.WriteLine($"X.B={X.B}");
+Console.WriteLine($"X.C={X.C}");
 
 static class X
 {
     public static int A = Sub(123) + C;
     public static int B { get; set; } = Sub(456);
-    public static int C = Sub(789);
+    public static int C = Sub(789) + A;
     public static int Sub(int s)
     {
         Console.WriteLine($"Sub {s}");

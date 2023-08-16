cd8fa9045599f230bf0cd2b6b8691ede20c9fc7f
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 11:26:30 +0900

    add StaticClass

---
+++ StaticClass/StaticClass/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+    if (i >= 3) A.MyProperty++;
+}
+Console.WriteLine($"A.MyProperty={A.MyProperty}");
+
+class A
+{
+    internal static int MyProperty { get; set; } = 100;
+    static A() => Console.WriteLine("A constructed");
+}
+class B
+{
+    internal static int MyProperty { get; set; } = 200;
+    static B() => Console.WriteLine("B constructed");
+}

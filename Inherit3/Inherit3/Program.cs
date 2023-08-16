ce318ac71b3f9c5b79f6d7bf12a979b4685521de
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 09:00:26 +0900

    add Inherit3

---
+++ Inherit3/Inherit3/Program.cs
@@ -0,0 +1,12 @@
+﻿var x = new B();
+Console.WriteLine($"x.MyProperty={x.MyProperty}");
+
+class A
+{
+    internal virtual int MyProperty { get; set; } = 1;
+    internal A() => Console.WriteLine($"A constructed MyProperty={MyProperty}");
+}
+class B : A
+{
+    internal override int MyProperty { get; set; } = 2;
+}

b0f10d2401fb7202fdfac2727f45128e4ad4f663
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 19:04:36 +0900

    add Fixed1

---
+++ Fixed1/Fixed1/Program.cs
@@ -0,0 +1,15 @@
+﻿var x1 = new A();
+var x2 = new B();
+
+class A
+{
+    public virtual string a { get; set; } = "A";
+    public virtual void Sub() => Console.WriteLine($"({a})");
+    public A() => Sub();
+}
+
+class B : A
+{
+    public override string a { get; set; } = "B";
+    public override void Sub() => Console.WriteLine($"[{a}]");
+}

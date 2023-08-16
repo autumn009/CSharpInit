4728675fe559a66acc1aa32d1f7b7ee010a40d14
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 11:55:02 +0900

    add SelfRefer

---
+++ SelfRefer/SelfRefer/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine($"B.a={B.a}");
+
+class A
+{
+    //public int a = b; // 書けない
+    public int b = 1;
+}
+static class B
+{
+    public static int a = b;
+    public static int b = 1;
+}

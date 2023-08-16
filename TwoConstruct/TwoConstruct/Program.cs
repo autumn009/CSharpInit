95829821986f12ec7ac689ef553f6650074f6baa
Akira Kawamata <autumn@piedey.co.jp>
2023-08-16 12:50:07 +0900

    add TwoConstruct

---
+++ TwoConstruct/TwoConstruct/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine($"A.X={A.X}");
+
+static class A
+{
+    internal static int X;
+    internal static int Y;
+    static A()
+    {
+        X = B.Z;
+        Y = 2;
+    }
+}
+static class B
+{
+    internal static int Z;
+    static B()
+    {
+        Z = A.Y;
+    }
+}

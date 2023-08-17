c59bb5f4cc0c05664b35025d5f92a1e33949c877
Akira Kawamata <autumn@piedey.co.jp>
2023-08-17 19:04:28 +0900

    add ModuleInitializer

---
+++ ModuleInitializer/ModuleInitializer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Running without A or B");
+
+static class A
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        Console.WriteLine("Init A called");
+    }
+    static A() => Console.WriteLine("Constructor A called");
+}
+static class B
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        Console.WriteLine("Init B called");
+    }
+    static B() => Console.WriteLine("Constructor B called");
+}

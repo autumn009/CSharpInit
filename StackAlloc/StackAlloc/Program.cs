021c63614d2f466504e89c39836c15313868b9e4
Akira Kawamata <autumn@piedey.co.jp>
2023-08-17 14:46:06 +0900

    add StackAlloc

---
+++ StackAlloc/StackAlloc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+Sub();
+
+[SkipLocalsInit]
+void Sub()
+{
+    unsafe
+    {
+        var buffer = stackalloc int[3];
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(buffer[i]);
+        }
+    }
+}

0aab18b9e439cab00b5ce5fec97151c702ef3a3f
Akira Kawamata <autumn@piedey.co.jp>
2023-08-17 13:35:58 +0900

    add Fixed2a

---
+++ Fixed2a/Fixed2a/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"A:{AB.MyPropertyB}");
+
+static class AB
+{
+    private const int initialValue = 123;
+    public static int MyPropertyA { get; set; } = initialValue;
+    public static int MyPropertyB { get; set; } = initialValue;
+    static AB()
+    {
+        Console.WriteLine($"B:{MyPropertyB}");
+        Console.WriteLine($"C:{MyPropertyA}");
+    }
+}
